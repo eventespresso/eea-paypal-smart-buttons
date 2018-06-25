f4c563c195621284052f870cbb0008f95c16715b
Josh Feck <josh@nxnweb.com>
2018-06-25 14:18:26 -0400

    Correct the version numbers

--- EE_Paypal_Smart_Buttons.class.php
+++ EE_Paypal_Smart_Buttons.class.php
@@ -34,7 +34,7 @@ class EE_Paypal_Smart_Buttons extends EE_Addon
             'Paypal_Smart_Buttons',
             array(
                 'version'                   => EE_PAYPAL_SMART_BUTTONS_VERSION,
-                'min_core_version' => '4.9.63.dev.000',
+                'min_core_version' => '4.9.64.rc.006',
                 'main_file_path'                => EE_PAYPAL_SMART_BUTTONS_PLUGIN_FILE,
                 'namespace'             => array(
                     'FQNS' => 'EventEspresso\PayPalSmartButtons',
