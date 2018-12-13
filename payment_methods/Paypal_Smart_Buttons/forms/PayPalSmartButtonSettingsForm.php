4ad24e6de249de6473ef33cf7117f19854ee3596
Mike Nelson <michael@eventespresso.com>
2018-12-13 12:59:33 -0800

    if there is an authentication error, give the user a better error message and a couple inline tips like the other paypal payment methods do

--- payment_methods/Paypal_Smart_Buttons/forms/PayPalSmartButtonSettingsForm.php
+++ payment_methods/Paypal_Smart_Buttons/forms/PayPalSmartButtonSettingsForm.php
@@ -110,8 +110,20 @@ class PayPalSmartButtonSettingsForm extends EE_Payment_Method_Form
                     )
                 );
             } catch (EE_Error $e) {
-                $this->add_validation_error(
-                    esc_html(
+                if($e->getMessage() === 'Unauthorized') {
+                    $message = esc_html__('PayPal did not accept your API credentials. Double-check your credentials and copy and paste them into the fields again and save changes. Alternatively, create a new app and try the new API credentials.', 'event_espresso');
+                    // Give them a few more tips inline.
+                    $this->get_input('PMD_debug_mode')->add_validation_error(
+                        esc_html__('If you are using PayPal Sandbox (test) credentials, Debug mode should be set to "Yes". Otherwise, if you are using live PayPal credentials, set this to "No".', 'event_espresso')
+                    );
+                    $this->get_input('client_id')->add_validation_error(
+                        esc_html__('Are you sure this is your REST API App Client ID, not your login username?%1$s', 'event_espresso')
+                    );
+                    $this->get_input('secret')->add_validation_error(
+                        esc_html__('Are you sure this is your REST API App Client ID, not your login password?', 'event_espresso')
+                    );
+                } else  {
+                    $message = esc_html(
                         sprintf(
                             _x(
                                 'Error validating PayPal credentials. %1$s',
@@ -120,8 +132,9 @@ class PayPalSmartButtonSettingsForm extends EE_Payment_Method_Form
                             ),
                             $e->getMessage()
                         )
-                    )
-                );
+                    );
+                }
+                $this->add_validation_error($message);
             }
         }
     }
